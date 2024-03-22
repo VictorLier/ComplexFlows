78043ce4e07fe5d7b703d856bdfccbf75e1d4fa0
jacoboep <s204408@student.dtu.dk>
2024-03-22 20:22:06 +0100

    Init tau matrices

--- Jacob/Project2/particles_sim_ooc.py
+++ Jacob/Project2/particles_sim_ooc.py
@@ -2,6 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pyglet
 
+# Make seed for reproducibility
+np.random.seed(1917)
+
 class particle():
     def __init__(self, radius = 20, position = np.array((0, 0)), velocity = np.array((0, 0)), angle = 0, omega = 0):
         
@@ -35,18 +38,17 @@ class particle():
         if self.position[1] > window_size[1] - self.radius :
             self.velocity[1] = -abs(self.velocity[1])
 
+
 class sim_particles():
-    def __init__(self, window_size = np.array((800, 800)), nparticles = 20, radius = 20, 
-                 max_velocity = 200, max_omega = 0.5, color1 = (20, 20, 250), 
-                 color2 = (200, 200, 250)):
+    def __init__(self, window_size = np.array((800, 800)), nparticles = 20, radius = 20):
         
         self.window_size = window_size
         self.nparticles = nparticles
         self.radius = radius
-        self.max_velocity = max_velocity
-        self.max_omega = max_omega
-        self.color1 = color1
-        self.color2 = color2
+        self.max_velocity = 200
+        self.max_omega = 0.5
+        self.color1 = (20, 20, 250)
+        self.color2 = (200, 200, 250)
         self.particles: list[particle] = []
         self.window_size = window_size
         self.make_particles()
@@ -59,8 +61,6 @@ class sim_particles():
         n = np.ceil(np.sqrt(self.nparticles))
         m = np.ceil(self.nparticles / n)
         self.xg, self.yg = np.meshgrid(np.arange(n)/(n-1), np.arange(m)/(m-1)) 
-        xg_f = self.xg.flatten()
-        yg_f = self.yg.flatten()
         # Make particles in a grid at least one diameter from walls with the use of the particle class
         position = np.zeros((self.nparticles, 2))
         for i in range(self.nparticles):
@@ -98,14 +98,6 @@ class sim_particles():
     def particle_collision(self):
         for i in range(self.nparticles-1):
             for j in range(i+1, self.nparticles):
-                # distance = np.sqrt(((self.position[j] - self.position[i])**2).sum())
-                # if  distance < 2 * self.radius:
-                #     # collision! - apply textbook eq. 5.14
-                #     n = (self.position[j] - self.position[i]) / distance
-                #     G0 = self.velocity[i] - self.velocity[j]
-                #     dvel = n * np.dot(n, G0)
-                #     self.velocity[i] -= dvel
-                #     self.velocity[j] += dvel
                 distance = np.sqrt(((self.particles[j].position - self.particles[i].position)**2).sum())
                 if  distance < 2 * self.radius:
                     # collision! - apply textbook eq. 5.14
@@ -114,6 +106,75 @@ class sim_particles():
                     dvel = n * np.dot(n, G0)
                     self.particles[i].velocity -= dvel
                     self.particles[j].velocity += dvel
+
+    def time_before_collision(self, particle_a: particle, particle_b: particle):
+        r1 = particle_a.radius
+        r2 = particle_b.radius
+        
+        x1 = particle_a.position
+        x2 = particle_b.position
+        v1 = particle_a.velocity
+        v2 = particle_b.velocity
+        
+        delta_x = x2 - x1
+        delta_v = v2 - v1
+        closing_speed_0 = np.linalg.norm(delta_v)
+        
+        if closing_speed_0 <= 0:
+            return np.NaN
+        
+        a = np.dot(delta_v, delta_v)
+        b = 2 * np.dot(delta_v, delta_x)
+        c = np.dot(delta_x, delta_x) - (r1 + r2) ** 2
+        
+        discriminant = b ** 2 - 4 * a * c
+        
+        if discriminant < 0:
+            return np.NaN
+        
+        t = (-b - np.sqrt(discriminant)) / (2 * a)
+        
+        if t < 0 or np.isnan(t):
+            return np.NaN
+        return t
+    
+    def time_before_collision_wall(self, particle: particle):
+        r = particle.radius
+        x = particle.position[0]
+        y = particle.position[1]
+        vx = particle.velocity[0]
+        vy = particle.velocity[1]
+        
+        if vx > 0:
+            t_x = (self.window_size[0] - r - x) / vx
+        elif vx < 0:
+            t_x = (r - x) / vx
+        else:
+            t_x = np.NaN
+        
+        if vy > 0:
+            t_y = (self.window_size[1] - r - y) / vy
+        elif vy < 0:
+            t_y = (r - y) / vy
+        else:
+            t_y = np.NaN
+        
+        if not np.isnan(t_y):
+            if not np.isnan(t_x):
+                return min(t_x, t_y)
+        return np.NaN
+    
+    def init_time_before_collision_matrix(self):
+        self.tau_particle_matrix = np.full((self.nparticles, self.nparticles), np.NaN)
+        for i in range(self.nparticles-1):
+            for j in range(i+1, self.nparticles):
+                tau = self.time_before_collision(self.particles[i], self.particles[j])
+                self.tau_particle_matrix[j,i] = tau
+        print(np.nanmin(self.tau_particle_matrix), np.nanargmin(self.tau_particle_matrix))
+        
+        self.tau_wall_matrix = np.full(self.nparticles, np.NaN)
+        for i in range(self.nparticles):
+            self.tau_wall_matrix[i] = self.time_before_collision_wall(self.particles[i])
 
     def update(self, dt):
         self.move_particles(dt)
@@ -126,12 +187,13 @@ class sim_particles():
         self.wall_collision()
         self.particle_collision()
 
-
-
 if __name__ == '__main__':
-    sim = sim_particles()
+    _window_size = np.array((800, 800))
+    _nparticles = 10
+    _radius = 20
+    sim = sim_particles(_window_size, _nparticles, _radius)
 
-    if True:
+    if False:
         window = pyglet.window.Window(sim.window_size[0], sim.window_size[1])
         batch = pyglet.graphics.Batch()
 
@@ -148,4 +210,8 @@ if __name__ == '__main__':
 
         del window
         del batch
+
+    if True:
+        sim.init_time_before_collision_matrix()
+        stop = True
             
